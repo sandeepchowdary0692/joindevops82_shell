3cbd27f0652c01c515a95b07e6c3b16b11dfa6b1
Sandeep-Chowdary0692 <sandeep.chowdary0692@gmail.com>
2025-01-05 21:24:15 +0530

    condition

--- 09-condition.sh
+++ 09-condition.sh
@@ -1,12 +1,12 @@
 $!/bin/bash
 
 NUMBER=$1
+
 # -gt (Greater-than), -lt (Less-than), -eq (equal), -ge (greater than equal), -le (Less than equal)
 
-if [$NUMBER -gt 100]
+if [ $NUMBER -gt 100 ]
 then
     echo "given number is greter then 100"
 else
-    echo "given number less then or equal 100" 
+    echo "given number less then or equal to 100" 
 fi
-
