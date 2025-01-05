969eb430d40e25bc289c4c9abad3e9d2a7c7d5b7
Sandeep-Chowdary0692 <sandeep.chowdary0692@gmail.com>
2025-01-05 14:26:32 +0530

    Array1

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -4,8 +4,8 @@ MOVIES=("Pushpa" "RRR" "Devara")
 
 #index starts from 0, size is 3
 
-echo "First movie: ${MOVIES[0]}"
-echo "Second movie: ${MOVIES[1]}"
-echo "Thried movie: ${MOVIES[2]}"
+echo "First movie:${#MOVIES[0]}"
+echo "Second movie:${#MOVIES[1]}"
+echo "Thried movie:${#MOVIES[2]}"
 
-echo "All movie: ${MOVIES[@]}"
+echo "All movie: ${#MOVIES[@]}"
