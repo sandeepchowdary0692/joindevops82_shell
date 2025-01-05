cfdf198907ca5c9cd835bcfbbd5816894242278f
Sandeep-Chowdary0692 <sandeep.chowdary0692@gmail.com>
2025-01-05 15:42:18 +0530

    conditions

---
+++ 09-condition.sh
@@ -0,0 +1,23 @@
+$!/bin/bash
+
+NUMBER=$1
+# -gt (Greater-than), -lt (Less-than), -eq (equal), -ge (greater than equal), -le (Less than equal)
+
+if [$NUMBER -gt 100]
+then
+    echo "given number is greter then 100"
+else
+    echo "given number less then or equal 100" 
+fi
+
+
+
+
+
+#a=5
+#b=15
+
+#if [$a -lt $b]
+#then
+ #  echo "a is less then b"
+#if
