477621d612a37db379c95f2011f513b37a419e3f
Sandeep-Chowdary0692 <sandeep.chowdary0692@gmail.com>
2025-01-05 11:43:52 +0530

    rename the 01 file

--- 03-variable.sh
+++ 03-variable.sh
@@ -3,8 +3,8 @@
 PERSON1=Raja #no space here before and after =
 PERSON2=Sandeep
 
-echo "$PERSON1:: Hi PERSON2"
-echo "$PERSON2:: Hello PERSON1"
+echo "$PERSON1:: Hi $PERSON2"
+echo "$PERSON2:: Hello $PERSON1"
 echo "$PERSON1:: How are you"
 echo "$PERSON2:: I am fine and what about you?"
 echo "$PERSON1:: i am fine"
