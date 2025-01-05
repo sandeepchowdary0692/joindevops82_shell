d08a1af0687955392fd06f9bb443019b4ce1f179
Sandeep-Chowdary0692 <sandeep.chowdary0692@gmail.com>
2025-01-05 15:13:17 +0530

    Special-vars1

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -6,6 +6,6 @@ echo "scrip name: $0"
 echo "present working directory: $PWD"
 echo "Home directory of current user: $HOME"
 echo "which user is running this script: $USER"
-cho "Process id of current script: $$"
+echo "Process id of current script: $$"
 sleep 60 &
 echo "Process id of last last command is background: $!"
