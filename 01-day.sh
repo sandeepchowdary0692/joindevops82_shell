2982742da256d482df11624bff0d9e540912141f
Sandeep-Chowdary0692 <sandeep.chowdary0692@gmail.com>
2025-01-05 11:13:18 +0530

    rename the 01 file

---
+++ 01-day.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Hellow world!"
+echo "Hellow world and welcome to our empire"
+echo "joindevops - Using Shebang in Bash Scripts"
--- 01.day.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Hellow world!"
-echo "Hellow world and welcome to our empire"
