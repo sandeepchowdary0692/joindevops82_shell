42405c0b179cf97237d6df47fb7e65915b515fc9
Sandeep-Chowdary0692 <sandeep.chowdary0692@gmail.com>
2025-01-05 14:20:24 +0530

    Array

---
+++ 07-arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MOVIES=("Pushpa""RRR""Devara")
+
+echo "First movie: ${MOVIES[0]}"
+echo "Second movie: ${MOVIES[1]}"
+echo "Thried movie: ${MOVIES[2]}"
+
+echo "All movie: ${MOVIES[@]}"
