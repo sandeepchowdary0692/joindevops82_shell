85d6dbdbee931caf9d6d70be216a1709aff1017c
Sandeep-Chowdary0692 <sandeep.chowdary0692@gmail.com>
2025-01-06 08:24:06 +0530

    condition-02

---
+++ 10-install-script.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0]
+then
+    echo "ERROR:: You must have sudo access to execute this script"
+    exit 1 #other than 0
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0]
+then #if not installed
+    
+dnf installed mysql -y
+    if [ $? -ne 0]
+        then
+            echo "installing MYSQL ......FAILUER"
+        else
+            echo "installing MYSQL ......SUCCESS"
+    fi
+else
+    echo "MYSQL is Already ......INSTALLED"
+fi
+
+
+dnf list installed git
+
+if [ $? -ne 0]
+then  
+    dnf install git -y
+    if [ $? -ne 0]
+    then 
+        echo "Installing Git ... FAILURE"
+        exit 1
+    else
+        echo   "Installing Git ... SUCCESS"
+    if
+else
+     echo "Git Already .... INSTALLED"
+fi
+        
+            
