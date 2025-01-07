f1239c457eaf2cb8e21fdb4ef9d0187b94085cbe
Sandeep-Chowdary0692 <sandeep.chowdary0692@gmail.com>
2025-01-07 10:38:37 +0530

    13-Logs.sh

--- 13-logs.sh
+++ 13-logs.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 USERID=$(id -u)
+
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
-N="\e[0m" #norml color
+N="\e[0m"
 
 LOGS_FOLDER="/var/log/shellscript-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1 ) 
